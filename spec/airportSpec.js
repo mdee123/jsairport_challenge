c21b20084e20ee8e97beb05fe4fcdd364bac0073
Mic <mic.cassano@gmail.com>
2016-02-16 12:05:38 +0000

    Add #takeOff functionality

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,25 +1,42 @@
 describe('Airport', function() {
-
   var airport;
 
-  beforeEach(function(){
+  beforeEach(function() {
     airport = new Airport();
     plane = {
-      land: function(){
-
+      land: function() {
+      },
+      takeOff: function() {
       }
     };
 
     spyOn(plane, 'land');
+    spyOn(plane, 'takeOff');
   });
 
-  describe('landing tests', function(){
-    it('instructs the plane to land', function(){
+  describe('#land', function() {
+    beforeEach(function() {
       airport.land(plane);
+    });
+    it('instructs the plane to land', function() {
       expect(plane.land).toHaveBeenCalled();
     });
-    // it('confirms the plane has landed', function(){
-    //   expect(airport)
-    // });
+
+    it('confirms the plane has landed', function() {
+      expect(airport.planes).toContain(plane);
+    });
+  });
+
+  describe('#takeOff', function() {
+    beforeEach(function() {
+      airport.takeOff(plane);
+    });
+    it('instructs the plane to land', function() {
+      expect(plane.takeOff).toHaveBeenCalled();
+    });
+
+    it('confirms the plane has landed', function() {
+      expect(airport.planes).not.toContain(plane);
+    });
   });
 });
--- src/airport.js
+++ src/airport.js
@@ -1,6 +1,18 @@
 function Airport() {
+  this.planes = [];
 }
 
 Airport.prototype.land = function(plane) {
   plane.land();
+  this.planes.push(plane);
+};
+
+Airport.prototype.takeOff = function (plane) {
+  plane.takeOff();
+  var arr = this.planes;
+  for(var i = 0; i <= arr.length - 1; i++) {
+    if(arr[i] === plane) {
+      arr.splice(i, 1);
+    }
+  }
 };
